fa70766f2ecbfb54608681d5c67a16b0acf28a2d
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-05-05 19:41:05 -0700

    4. Sets up package.json, webpack, and tsconfig

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    },
+    module: {
+        rules: [
+            { test: /\.(js|jsx| ts|tsx)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new CopyPlugin([
+            { from: './public/_redirects '}
+        ])
+    ],
+    mode: process.env_NODE_ENV === 'production' ? 'production' : 'development',
+    devServer: { historyApiFallback: true }
+};
