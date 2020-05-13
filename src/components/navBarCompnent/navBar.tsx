727d168eb789dddb6caf92b53bb4d3932899860a
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-05-13 12:05:51 -0700

    12. Remodels navbar

--- src/components/navBarCompnent/navBar.tsx
+++ src/components/navBarCompnent/navBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 interface Props {
     currentUser: string,
@@ -13,20 +15,25 @@ const NavBar: React.SFC<Props> = ({
     logout
 }: Props) => {
 
+    const element = <FontAwesomeIcon icon={ faBars } />
+
     return (
         <>
             <Navbar bg="light" expand="lg">
+                <NavDropdown title={ element } id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Support</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">TOS</NavDropdown.Item>
+                </NavDropdown>
                 <Navbar.Brand href="/">Pro Clips</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
+                    <Nav style={{ margin: '0 auto' }}>
                         <Nav.Link href="popularclips">Popular Clips</Nav.Link>
                         <Nav.Link href="browseclips">Browse</Nav.Link>
-                        <Nav.Link href="clipupload">Upload Clips</Nav.Link>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search creators or categories" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
+                    </Nav>
+                    <Nav style={{ marginRight: '10px' }}>
+                        <Nav.Link href="login">Login</Nav.Link>
+                        <Nav.Link href="register">Sign Up</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
