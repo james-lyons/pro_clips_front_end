2990e572389d8c75ba0fb4c0317f25bcff838833
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-07-09 23:08:54 -0700

    126. More style changes

--- src/components/Unavailable/UnavailableComp.tsx
+++ src/components/Unavailable/UnavailableComp.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const UnavailableComp: React.SFC<{}> = () => {
+
+    const theme = localStorage.getItem('theme');
+
     return (
         <div id='unavailable-container'>
-            <h2>Sorry, we couldn't find the page you are looking for.</h2>
+            <h2 id={ theme === 'dark' ? 'unavailable-message-dark' : 'unavailable-message-light' }>
+                Sorry, we couldn't find the page you are looking for.
+            </h2>
         </div>
     );
 };
