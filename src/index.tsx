cab885e40324e3bc7b68f595d5a3ac5657040eaa
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-07-11 16:45:21 -0700

    147. Adds history?

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import App from './App';
 import store from './store';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { createBrowserHistory } from "history";
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
     <Provider store={ store }>
-        <Router>
+        <Router history={ history }>
             <App />
         </Router>
     </Provider>, document.getElementById('root')
