fed3c976adbda028082116549795fc0a956c29e0
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-07-12 20:39:50 -0700

    149. Edits previous updates

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js',
+        filename: 'bundle.js',
         publicPath: '/'
     },
     resolve: {
